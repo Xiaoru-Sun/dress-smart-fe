b1e18171051928634b8e2f0ea68f36c1ac303819
Xiaoru Sun <xiaorusun@Xiaorus-MacBook-Air.local>
2024-09-11 12:56:42 +0100

    adds detect click outside to close subcategory dropdown

--- src/components/CategoryCard.tsx
+++ src/components/CategoryCard.tsx
@@ -1,14 +1,57 @@
+import { useEffect, useRef, useState } from "react";
+
 type CategoryCardProp = {
-  categoryObject: { category: string };
+  categoryObject: { category: string; subCategory: string[] };
 };
 
 const CategoryCard = ({ categoryObject }: CategoryCardProp) => {
+  const [toggled, setToggled] = useState<boolean>(false);
+  const handleToggle = () => {
+    setToggled(!toggled);
+  };
+
+  const dropDownRef = useRef(null);
+
+  useEffect(() => {
+    const handler = (e) => {
+      console.log("target", e.target);
+      if (dropDownRef.current) {
+        if (!dropDownRef.current.contains(e.target)) {
+          setToggled(false);
+        }
+      }
+    };
+    document.addEventListener("click", handler);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
+
   return (
     <>
-      <div className="ml-4 text-secondary font-semibold">
-        {categoryObject.category}
+      <div
+        className="w-[80%] flex flex-col items-start justify-center"
+        ref={dropDownRef}
+      >
+        <button
+          className="w-full h-full flex justify-between ml-4 text-secondary font-semibold"
+          onClick={handleToggle}
+        >
+          <span> {categoryObject.category}</span>
+          <span>{toggled ? "-" : "+"}</span>
+        </button>
       </div>
-      <div></div>
+
+      {categoryObject.subCategory.map((subCategoryObject, index) => {
+        return (
+          <div
+            className={`text-primary ${toggled ? "visible" : "hidden"}`}
+            key={index}
+          >
+            {subCategoryObject} {categoryObject.category}
+          </div>
+        );
+      })}
     </>
   );
 };
--- src/pages/Wardrobe.tsx
+++ src/pages/Wardrobe.tsx
@@ -1,27 +1,49 @@
+import { useEffect, useRef } from "react";
 import CategoryCard from "../components/CategoryCard";
 
 const Wardrobe = () => {
+  const dropDownRef = useRef(null);
+
+  const handler = (e) => {
+    console.log("target", e.target);
+    if (dropDownRef.current) {
+      if (!dropDownRef.current.contains(e.target)) {
+        setToggled(false);
+      }
+    }
+  };
+  useEffect(() => {
+    document.addEventListener("click", handler);
+    return () => {
+      document.removeEventListener("click", handler);
+    };
+  }, []);
+
   const wardrobeDummyData = [
     {
       id: 1,
       category: "Coats",
-      subCategory: ["Smart Coats", "Casual Coats", "Winter Coats"],
+      subCategory: ["Smart", "Casual", "Winter"],
     },
-    { id: 2, category: "Dresses" },
-    { id: 3, category: "Skirts" },
-    { id: 4, category: "Trousers" },
-    { id: 5, category: "Jeans" },
-    { id: 6, category: "Shoes" },
-    { id: 7, category: "Accessories" },
+    { id: 2, category: "Dresses", subCategory: ["Smart", "Casual", "Winter"] },
+    { id: 3, category: "Skirts", subCategory: ["Smart", "Casual", "Winter"] },
+    { id: 4, category: "Trousers", subCategory: ["Smart", "Casual", "Winter"] },
+    { id: 5, category: "Jeans", subCategory: ["Smart", "Casual", "Winter"] },
+    { id: 6, category: "Shoes", subCategory: ["Smart", "Casual", "Winter"] },
+    {
+      id: 7,
+      category: "Accessories",
+      subCategory: ["Smart", "Casual", "Winter"],
+    },
   ];
   return (
-    <div className="flex flex-col items-center justify-center w-full h-screen">
-      <h1 className="text-[16px] text-secondary font-bold">Wardrobe</h1>
-      <div className="h-[87%] flex flex-col w-[85%] justify-evenly ">
+    <div className="w-full h-screen flex flex-col items-center justify-start">
+      <h1 className="mt-4 text-[16px] text-secondary font-bold">Wardrobe</h1>
+      <div className="h-[85%] w-[85%] flex flex-col justify-evenly ">
         {wardrobeDummyData.map((categoryObject, index) => {
           return (
             <div
-              className="h-[11%] mt-1 mb-1 rounded-lg border-secondary border-solid border-2 flex items-center"
+              className="h-[10%] rounded-lg border-secondary border-solid border-2 flex items-center"
               key={index}
             >
               <CategoryCard categoryObject={categoryObject} />
